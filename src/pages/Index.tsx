703f82886899f2141acc8f892a7712fafc25cb17
BackRunner <dev@backrunner.top>
2025-05-10 01:55:24 +0800

    feat: theme toggle position changed to absolute

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,11 +1,10 @@
-
 import DiffViewer from "@/components/DiffViewer";
 import { ThemeToggle } from "@/components/ThemeToggle";
 
 const Index = () => {
   return (
     <>
-      <div className="fixed top-4 right-4 z-50 animate-fade-in">
+      <div className="absolute top-4 right-4 z-50 animate-fade-in">
         <ThemeToggle />
       </div>
       <div className="min-h-screen bg-gradient-to-b from-background to-background/95 dark:from-background dark:to-background/95 pb-20">
